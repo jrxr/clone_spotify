2aaf30f22076f5fc0639aba5af7f5ab66dce27c2
Agacy junior <juniormartinssom@gmail.com>
2022-03-09 13:07:51 -0300

    Implementing Middleware

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,12 +6,22 @@ import {
   PlusCircleIcon,
   RssIcon,
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react';
 
 export default function Sidebar() {
+  const { data: session, status } = useSession(); 
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
+        <button 
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log out</p>
+        </button>
+        <button 
+          className="flex items-center space-x-2 hover:text-white"
+        >
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
         </button>
